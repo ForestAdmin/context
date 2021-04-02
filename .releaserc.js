e9bed8bf535a8f5ff8e880dae7f6737d9e4ba305
Sliman <smedini@gmail.com>
2021-04-02 16:09:34 +0200

    chore: remove slack integration

--- .releaserc.js
+++ .releaserc.js
@@ -7,41 +7,5 @@ module.exports = {
     '@semantic-release/npm',
     '@semantic-release/git',
     '@semantic-release/github',
-    [
-      'semantic-release-slack-bot',
-      {
-        markdownReleaseNotes: true,
-        notifyOnSuccess: true,
-        notifyOnFail: false,
-        onSuccessTemplate: {
-          text: "📦 $package_name@$npm_package_version has been released!",
-          blocks: [{
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: '*New `$package_name` package released!*'
-            }
-          }, {
-            type: 'context',
-            elements: [{
-              type: 'mrkdwn',
-              text: "📦  *Version:* <$repo_url/releases/tag/v$npm_package_version|$npm_package_version>"
-            }]
-          }, {
-            type: 'divider',
-          }],
-          attachments: [{
-            blocks: [{
-              type: 'section',
-              text: {
-                type: 'mrkdwn',
-                text: '*Changes* of version $release_notes',
-              },
-            }],
-          }],
-        },
-        packageName: '@forestadmin/context',
-      }
-    ],
   ],
 }
