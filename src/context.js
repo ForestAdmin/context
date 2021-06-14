66727c131f3e4a508f4c2d5033524707c83aee58
slimane <slimanm@forestadmin.com>
2021-06-14 17:41:33 +0200

    feat: addStep feature (#4)

--- src/context.js
+++ src/context.js
@@ -1,61 +1,16 @@
-const Plan = require('./plan');
 const Metadata = require('./metadata');
 
 module.exports = class Context {
   constructor() {
     this._bag = {};
     this._bag.assertPresent = this._assertPresent.bind(this);
-    this._privates = [];
     this._metadata = new Metadata();
   }
 
-  static newPlan() {
-    return new Plan();
-  }
+  get() { return this._bag; }
 
-  static execute(plan, context = new Context()) {
-    if (!plan) throw new Error('missing plan');
-    const planIsABuildPlan = plan instanceof Plan;
-    const planIsAFunction = typeof plan === 'function';
-    const planIsAnArray = Array.isArray(plan);
-    const planIsInvalid = !planIsAFunction && !planIsAnArray && !planIsABuildPlan;
-
-    if (planIsABuildPlan) {
-      plan.deliverSteps()
-        .forEach((step) => Context.execute(step.plan, context));
-    } else if (planIsAFunction) {
-      plan(context);
-      context.flushPrivates();
-    } else if (planIsAnArray) {
-      plan.forEach((buildPlan) => Context.execute(buildPlan, context));
-    } else if (planIsInvalid) {
-      throw new Error(`Invalid plan: received '${typeof plan}' instead of 'Plan', 'function' or 'Array'`);
-    }
-
-    return context.get();
-  }
-
-  static init(buildPlan) {
-    Context.execute(buildPlan, Context.instance = new Context());
-  }
-
-  static getInstance() {
-    if (!Context.instance) throw new Error('Context not initiated');
-    return Context.instance;
-  }
-
-  static inject() {
-    return Context.getInstance().get();
-  }
-
-  static _getInstanceName(Class, { name } = {}) {
-    if (name) return name;
-    const className = Class.name;
-    return className.charAt(0).toLowerCase() + className.slice(1);
-  }
-
-  executePlan(plan) {
-    return Context.execute(plan, this);
+  getMetadata() {
+    return this._metadata.get();
   }
 
   _assertPresent(requisites) {
@@ -68,16 +23,23 @@ module.exports = class Context {
     return true;
   }
 
-  get() { return this._bag; }
-
-  flushPrivates() {
-    this._privates.forEach((name) => delete this._bag[name]);
-    this._privates = [];
+  openStep(path, name, options) {
+    this._metadata.add(path, name, 'step', null, options);
   }
 
-  _setValue(name, value, options = {}) {
+  closeStep(path) {
+    this.flushPrivates(path);
+  }
+
+  flushPrivates(path) {
+    [
+      ...this._metadata.findPrivateValuesInStep(path),
+      ...this._metadata.findValuesInPrivateSubSteps(path),
+    ].forEach((name) => delete this._bag[name]);
+  }
+
+  _setValue(name, value) {
     this._bag[name] = value;
-    if (options.private) this._privates.push(name);
     return this;
   }
 
@@ -86,63 +48,47 @@ module.exports = class Context {
     this._setValue(name, value, options);
   }
 
-  addValue(name, value, options) {
-    this._metadata.add(name, 'value');
+  addValue(path, name, value, options) {
+    this._metadata.add(path, name, 'value', value, options);
     this._setNewValue(name, value, options);
     return this;
   }
 
-  addInstance(name, instance, options) {
-    this._metadata.add(name, 'instance');
+  addInstance(path, name, instance, options) {
+    this._metadata.add(path, name, 'instance', instance, options);
     this._setNewValue(name, instance, options);
     return this;
   }
 
-  addFunction(name, value, options) {
-    this._metadata.add(name, 'function');
-    this._setNewValue(name, value, options);
+  addFunction(path, name, theFunction, options) {
+    this._metadata.add(path, name, 'function', theFunction, options);
+    this._setNewValue(name, theFunction, options);
     return this;
   }
 
-  /**
-   * @deprecated Use addUsingClass instead.
-   */
-  addClass(Class, options) {
-    const name = Context._getInstanceName(Class, options);
-    this._metadata.add(name, 'class');
-    const instance = this._instanciate(Class, options);
-    this._setNewValue(name, instance, options);
-    return this;
-  }
-
-  addFactoryFunction(name, factoryFunction, options) {
-    this._metadata.add(name, 'function*');
+  addFactoryFunction(path, name, factoryFunction, options) {
+    this._metadata.add(path, name, 'function*', factoryFunction, options);
     const bag = this.get();
     const theFunction = factoryFunction(bag);
     this._setNewValue(name, theFunction, options);
     return this;
   }
 
-  addFactory(name, factory) {
-    this._metadata.add(name, 'factory');
-    const value = factory(this.get());
-    this._setNewValue(name, value);
-    return this;
-  }
-
-  addUsingClass(name, Class, options) {
-    this._metadata.add(name, 'class');
+  addUsingClass(path, name, Class, options) {
+    this._metadata.add(path, name, 'class', Class, options);
     const instance = this._instanciate(Class, options);
     this._setNewValue(name, instance, options);
     return this;
   }
 
-  addModule(name, module, options) { return this.addValue(name, module, options); }
+  addModule(path, name, module, options) {
+    this._metadata.add(path, name, 'module', module, options);
+    this._setNewValue(name, module, options);
+    return this;
+  }
 
-  addAllKeysFrom(object, options) {
-    Object
-      .entries(object)
-      .forEach(([name, value]) => this.addValue(name, value, options));
+  with(name, work) {
+    work(this._lookup(name));
     return this;
   }
 
@@ -157,22 +103,7 @@ module.exports = class Context {
     return { ...bag, ...map(bag) };
   }
 
-  invokeFactory(factoryId, instanceKey, parameters) {
-    const factoryMethod = this.lookup(factoryId);
-    const value = factoryMethod(instanceKey, parameters);
-    return this.addValue(instanceKey, value);
-  }
-
-  with(name, work) {
-    work(this.lookup(name));
-    return this;
-  }
-
-  replace(name, value) {
-    return this._setValue(name, value);
-  }
-
-  lookup(name) {
+  _lookup(name) {
     const bag = this.get();
     if (Array.isArray(name)) {
       const dependanciesArray = name.map((key) => bag[key]);
@@ -183,9 +114,5 @@ module.exports = class Context {
       return dependanciesObject;
     }
     return bag[name];
-  }
-
-  getMetadata() {
-    return this._metadata.get();
   }
 };
