7b72cd966be2545ad0fcd3d2244fa764ea73b5fd
slimane <smedini@gmail.com>
2023-02-03 17:18:29 +0100

    chore(force-release): define force-release

--- .releaserc.js
+++ .releaserc.js
@@ -1,6 +1,17 @@
 module.exports = {
   branches: ["main"],
   plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        'releaseRules': [
+          // This rule allow to force a release by adding "force-release" in scope.
+          // Example: `chore(force-release): migrate widgets to new format`
+          // Source: https://github.com/semantic-release/commit-analyzer#releaserules
+          { scope: 'force-release', release: 'patch' },
+        ],
+      },
+    ],
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
